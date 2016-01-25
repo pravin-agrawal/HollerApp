cd661d9bc52fa3a235dc8844d2c6a44c5fd8affc
raviBhai <agrawal.ravi1989@gmail.com>
2016-01-25 08:40:25 +0530

    Adding parent project

--- holler-web/src/main/java/com/holler/controller/LoginLogoutController.java
+++ holler-web/src/main/java/com/holler/controller/LoginLogoutController.java
@@ -2,6 +2,7 @@ package com.holler.controller;
 
 
 
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.holler.bean.LoginDTO;
+import com.holler.holler_dao.common.HollerConstants;
 import com.holler.holler_service.UserService;
 
 
@@ -32,6 +34,13 @@ public class LoginLogoutController {
 	@RequestMapping(value="/signOutUser", method=RequestMethod.POST)
 	public @ResponseBody Map<String, String> logoutUser(HttpServletRequest request){
 		Map<String, String> result = userService.signOutUser(request);
+		return result;
+	}
+	
+	@RequestMapping(value="/testHit", method=RequestMethod.POST)
+	public @ResponseBody Map<String, Object> testHit(HttpServletRequest request){
+		Map<String, Object> result = new HashMap<String, Object>();
+		result.put("testHit", HollerConstants.SUCCESS);
 		return result;
 	}
 	
