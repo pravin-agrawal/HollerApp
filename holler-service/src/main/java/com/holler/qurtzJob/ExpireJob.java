63ad6b497b853767ecbe351f087bb786ecc7e345
pravin-tal <pravin.agrawal@fidelis-inc.com>
2016-12-04 19:01:15 +0530

    code added to implement quartz jobs and externalise xml properties.

---
+++ holler-service/src/main/java/com/holler/qurtzJob/ExpireJob.java
@@ -0,0 +1,14 @@
+package com.holler.qurtzJob;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+/**
+ * Created by pravina on 23/11/16.
+ */
+public class ExpireJob implements Job {
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+
+    }
+}
